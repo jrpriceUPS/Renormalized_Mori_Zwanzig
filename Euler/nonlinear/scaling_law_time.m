b342276833cf77a6635a5600b8e07d45a5da1516
Jake Price <jrp14@uw.edu>
2018-03-24 08:47:05 -0700

    updated time scaling law notes

--- Euler/nonlinear/scaling_law_time.m
+++ Euler/nonlinear/scaling_law_time.m
@@ -27,22 +27,22 @@ function coefficients = scaling_law_time(N,degree)
 
 coefficients = zeros(degree,1);
 
-if degree == 1 % updated 3/23/2018
+if degree == 1 % updated 3/24/2018 N=48
     coefficients = exp(0.346140204693076) * N^-1.100982456346863;
 end
 
-if degree == 2 % updated 3/23/2018
+if degree == 2 % updated 3/24/2018 N=48
     coefficients(1) = exp(0.450485290588850) * N^-0.910866820408359;
     coefficients(2) = exp(0.241869704035654) * N^-1.812532078606838;
 end
 
-if degree == 3 % updated 3/23/2018
+if degree == 3 % updated 3/24/2018 N=48
     coefficients(1) = exp(0.700418137617023) * N^-0.935138605878111;
     coefficients(2) = exp(1.217147112806629) * N^-1.962577643260228;
     coefficients(3) = exp(1.431162474047888) * N^-3.188011710973634;
 end
 
-if degree == 4 % updated 3/23/2018
+if degree == 4 % updated 3/24/2018 N=48
     coefficients(1) = exp(0.898823727373543) * N^-0.988857926218814;
     coefficients(2) = exp(1.753386619806672) * N^-2.103964030340424;
     coefficients(3) = exp(2.388790403108812) * N^-3.415262749214611;
