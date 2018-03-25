daa68e3e2fa0a166238e470171df182c85223d6d
Jake Price <jrp14@uw.edu>
2018-03-24 23:36:34 -0700

    edited spectra code to work with chosen variable names

--- Euler/simulations/multiple_renormalized_models_Mar_19/create_spectra.m
+++ Euler/simulations/multiple_renormalized_models_Mar_19/create_spectra.m
@@ -7,18 +7,16 @@ for i = 1:length(N_list)
     load(sprintf('u_array4_%i.mat',N))
     load(sprintf('t4_%i.mat',N))
     
-    u_current = squeeze(u(:,:,:,:,:,end));
-    u_full = u_fullify(u_current,M);
-    for j = 1:length(t)
-        u_current = squeeze(u(:,:,:,:,:,j));
+    for j = 1:length(t4)
+        u_current = squeeze(u_array4(:,:,:,:,:,j));
         u_full = u_fullify(u_current,M);
-    [spectrum,k_list] = energy_spectrum(u_full,M);
-    figure(1)
-    hold off
-    plot(log(k_list),log(spectrum),'.','markersize',20)
-    title(sprintf('Energy Spectrum for N = %i at t = %i',N,t(j)),'fontsize',16)
-    xlabel('log(|k|^2)','fontsize',16)
-    ylabel('log(energy)','fontsize',16)
-    saveas(gcf,sprintf('spectrum%i_%i',N,j),'png')
+        [spectrum,k_list] = energy_spectrum(u_full,M);
+        figure(1)
+        hold off
+        plot(log(k_list),log(spectrum),'.','markersize',20)
+        title(sprintf('Energy Spectrum for N = %i at t = %i',N,t4(j)),'fontsize',16)
+        xlabel('log(|k|^2)','fontsize',16)
+        ylabel('log(energy)','fontsize',16)
+        saveas(gcf,sprintf('spectrum%i_%i',N,j),'png')
     end
 end
