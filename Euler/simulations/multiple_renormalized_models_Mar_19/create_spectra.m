d9b3c06aa3149aa66e7cd25165ea9c57db3f7199
Jake Price <jrp14@uw.edu>
2018-04-02 14:18:17 -0700

    updated plotting

--- Euler/simulations/multiple_renormalized_models_Mar_19/create_spectra.m
+++ Euler/simulations/multiple_renormalized_models_Mar_19/create_spectra.m
@@ -1,4 +1,4 @@
-function create_spectra(N_list,skip)
+function create_spectra(N_list,skip,yes_log)
 
 format long
 close all
@@ -24,14 +24,26 @@ for i = 1:length(N_list)
         u_full = u_fullify(u_current,M);
         [spectrum,k_list] = energy_spectrum(u_full,M);
         
-        figure(1)
-        hold off
-        plot(log(k_list),log(spectrum),'.','markersize',20)
-        title(sprintf('Energy Spectrum for N = %i at t = %i',N,t4(j)),'fontsize',16)
-        xlabel('log(|k|)','fontsize',16)
-        ylabel('log(energy)','fontsize',16)
-        axis([0,log(N),-10,5])
-        saveas(gcf,sprintf('spect%i/spectrum%i_%i',N,N,count),'png')
-        count = count+1;
+        if yes_log
+            figure(1)
+            hold off
+            plot(log(k_list),log(spectrum),'.','markersize',20)
+            title(sprintf('Energy Spectrum for N = %i at t = %i',N,t4(j)),'fontsize',16)
+            xlabel('log(|k|)','fontsize',16)
+            ylabel('log(energy)','fontsize',16)
+            axis([0,log(N),-10,5])
+            saveas(gcf,sprintf('spect%i/spectrum%i_%i',N,N,count),'png')
+            count = count+1;
+        else
+            figure(1)
+            hold off
+            plot(k_list,spectrum,'.','markersize',20)
+            title(sprintf('Energy Spectrum for N = %i at t = %i',N,t4(j)),'fontsize',16)
+            xlabel('|k|','fontsize',16)
+            ylabel('log(energy)','fontsize',16)
+            axis([0,N,0,1/4])
+            saveas(gcf,sprintf('spect%i/spectrum%i_%i',N,N,count),'png')
+            count = count+1;
+        end
     end
 end
