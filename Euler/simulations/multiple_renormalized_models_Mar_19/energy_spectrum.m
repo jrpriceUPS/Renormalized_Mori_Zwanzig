98961edbd08a382cb4ecf13e54ef066507f2d0c5
Jake Price <jrp14@uw.edu>
2018-04-02 13:45:24 -0700

    energy spectrum update

--- Euler/simulations/multiple_renormalized_models_Mar_19/energy_spectrum.m
+++ Euler/simulations/multiple_renormalized_models_Mar_19/energy_spectrum.m
@@ -10,12 +10,12 @@ k(:,:,:,3) = kz;
 
 k_mag = sum(k.^2,4);
 
-u_mag = sum(u_full.*conj(u_full),4);
+u_mag = sqrt(sum(u_full.*conj(u_full),4));
 
-k_list = 1:(M/3)^2*3;
+k_list = 1:M/3;
 spectrum = zeros(length(k_list),1);
 
 for i = 1:length(k_list)
     current_k = k_list(i);
-    spectrum(i) = sum(u_mag(k_mag == current_k));
+    spectrum(i) = sum(u_mag(current_k-1/2 < k_mag & k_mag < current_k+1/2));
 end
