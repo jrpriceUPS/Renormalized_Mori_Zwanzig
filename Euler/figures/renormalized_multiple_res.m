dc9c3959a69b3a343c1df5af7b1218b668c6f90e
Jake Price <jrp14@uw.edu>
2018-04-13 16:24:18 -0700

    changed scaling laws!

---
+++ Euler/figures/renormalized_multiple_res.m
@@ -0,0 +1,189 @@
+function [slopes,slopes2,turn_times,ens_max,ens_max_time,vort_max,vort_max_time] = renormalized_multiple_res(N_list,end_time,filetype)
+
+format long
+close all
+
+addpath ../../simulation_functions
+addpath ../../nonlinear
+addpath ../../analysis
+
+colors = linspecer(length(N_list),'qualitative');
+
+slopes = zeros(length(N_list),1);
+slopes2 = zeros(length(N_list),1);
+turn_times = zeros(length(N_list),1);
+ens_max = zeros(length(N_list),1);
+ens_max_time = zeros(length(N_list),1);
+vort_max = zeros(length(N_list),1);
+vort_max_time = zeros(length(N_list),1);
+
+for i = 1:length(N_list)
+    
+    N = N_list(i);
+    full_legend{i} = sprintf('Fourth order N = %i ROM',N);
+    
+end
+
+for i = 1:length(N_list)
+    
+    N = N_list(i);
+    
+    for j = 1:i
+        leg_sw{j} = full_legend{j};
+        leg_se{j} = full_legend{j};
+        leg_ne{j} = full_legend{j};
+    end
+    
+    leg_sw{i+1} = 'location';
+    leg_sw{i+2} = 'southwest';
+    leg_se{i+1} = 'location';
+    leg_se{i+2} = 'southeast';
+    leg_ne{i+1} = 'location';
+    leg_ne{i+2} = 'northeast';
+    
+    M = 3*N;
+    
+    % uniform grid
+    x = linspace(0,2*pi*(2*M-1)/(2*M),2*M).';
+    y = x;
+    z = x;
+    
+    % 3D array of data points
+    [X,Y,Z] = ndgrid(x,y,z);
+    
+    % create initial condition
+    eval = taylor_green(X,Y,Z);
+    u_full = fftn_norm(eval);
+    u = u_squishify(u_full,N);
+    
+    % make k array
+    k_vec = [0:M-1,-M:1:-1];
+    [kx,ky,kz] = ndgrid(k_vec,k_vec,k_vec);
+    k = zeros(2*M,2*M,2*M,3);
+    k(:,:,:,1) = kx;
+    k(:,:,:,2) = ky;
+    k(:,:,:,3) = kz;
+    
+    params.k = k;
+    params.N = N;
+    params.M = M;
+    params.a = 2:M;
+    params.b = 2*M:-1:M+2;
+    params.a_tilde = N+1:M;
+    params.a_tilde2 = 2*N+1:M;
+    params.print_time = 1;
+    params.no_time = 1;
+    
+    
+    params4 = params;
+    params4.func = @(x) t4model_RHS(x);
+    params4.coeff = scaling_law(N,4);
+    
+    if exist(sprintf('u_array4_%i_%i.mat',N,end_time),'file') == 2
+        
+        load(sprintf('u_array4_%i_%i.mat',N,end_time))
+        load(sprintf('t4_%i_%i',N,end_time))
+        
+    else
+        
+        % run the simulation
+        options = odeset('RelTol',1e-10,'Stats','on','InitialStep',1e-3);
+        [t4,u_raw4] = ode45(@(t,u) RHS(u,t,params4),[0,end_time],u(:),options);
+        
+        
+        % reshape the output array into an intelligible shape (should make this a
+        % separate function later)
+        u_array4 = zeros([size(u) length(t4)]);
+        for l = 1:length(t4)
+            u_array4(:,:,:,:,:,l) = reshape(u_raw4(l,:),[N,N,N,3,4]);
+        end
+        
+        save(sprintf('t4_%i_%i',N,end_time),'t4');
+        save(sprintf('u_array4_%i_%i.mat',N,end_time),'u_array4');
+        
+    end
+    
+    % plot the energy in some modes
+    energy = get_3D_energy(u_array4,N);
+    save(sprintf('energy_%i_%i',N,end_time),'energy')
+    figure(1)
+    hold on
+    plot(log(t4),log(energy),'linewidth',2,'color',colors(i,:))
+    legend(leg_sw{:})
+    title('Energy in resolved modes','fontsize',16)
+    xlabel('log(time)','fontsize',16)
+    ylabel('log(energy)','fontsize',16)
+    saveas(gcf,sprintf('energy_mult_%i',N),filetype)
+    
+    energy_change = abs(energy-energy(1))/abs(energy(1));
+    turn_times(i) = t4(find(energy_change>0.10,1));
+
+    
+    stop_recording = log(t4(find(energy_change>0.90,1)));
+    s = polyfit(log(t4(log(t4)>turn_times(i)&log(t4)<stop_recording)),log(energy(log(t4)>turn_times(i)&log(t4)<stop_recording)),1);
+    slopes(i) = s(1);
+    
+    second_wind = log(t4(find(energy_change>0.99,1)));
+    s2 = polyfit(log(t4(log(t4)>second_wind)),log(energy(log(t4)>second_wind)),1);
+    slopes2(i) = s2(1);
+    
+    figure(2)
+    hold on
+    plot(log(t4),log(energy),'linewidth',2,'color',colors(i,:))
+    plot(log(t4(find(energy_change>0.10,1))),log(energy(find(energy_change>0.10,1))),'o','markersize',20,'color',colors(i,:))
+    plot(log(t4(find(energy_change>0.90,1))),log(energy(find(energy_change>0.90,1))),'o','markersize',20,'color',colors(i,:))
+    plot(log(t4(find(energy_change>0.99,1))),log(energy(find(energy_change>0.99,1))),'s','markersize',20,'color',colors(i,:))
+    title('Energy in resolved modes','fontsize',16)
+    xlabel('log(time)','fontsize',16)
+    ylabel('log(energy)','fontsize',16)
+    saveas(gcf,sprintf('energy_mult_slopes_%i',N),filetype)
+    
+    
+    
+    ens = enstrophy(u_array4);
+    [m,peak_time] = max(ens);
+    ens_max(i) = m;
+    ens_max_time(i) = t4(peak_time);
+    
+    figure(3)
+    hold on
+    plot(t4,ens,'linewidth',2,'color',colors(i,:))
+    legend(leg_ne{:})
+    title('Enstrophy','fontsize',16)
+    xlabel('time','fontsize',16)
+    ylabel('enstrophy','fontsize',16)
+    saveas(gcf,sprintf('enstrophy_mult_%i',N),filetype)
+    
+    figure(4)
+    hold on
+    plot(t4(t4<=100),ens(t4<=100),'linewidth',2,'color',colors(i,:))
+    legend(leg_ne{:})
+    title('Enstrophy','fontsize',16)
+    xlabel('time','fontsize',16)
+    ylabel('enstrophy','fontsize',16)
+    saveas(gcf,sprintf('enstrophy_mult_trim%i',N),filetype)
+    
+    [~,vort2] = vorticity(u_array4);
+    [m,peak_time] = max(vort2);
+    vort_max(i) = m;
+    vort_max_time(i) = t4(peak_time);
+    
+    figure(5)
+    hold on
+    plot(t4,vort2,'linewidth',2,'color',colors(i,:))
+    legend(leg_ne{:})
+    title('Maximum of vorticity','fontsize',16)
+    xlabel('time','fontsize',16)
+    ylabel('Maximal vorticity','fontsize',16)
+    saveas(gcf,sprintf('vorticity_mult_%i',N),filetype)
+    
+    figure(6)
+    hold on
+    plot(t4(t4<=100),vort2(t4<=100),'linewidth',2,'color',colors(i,:))
+    legend(leg_ne{:})
+    title('Maximum of vorticity','fontsize',16)
+    xlabel('time','fontsize',16)
+    ylabel('maximal vorticity','fontsize',16)
+    saveas(gcf,sprintf('vorticity_mult_trim%i',N),filetype)
+    
+end
--- Euler/nonlinear/scaling_law.m
+++ Euler/nonlinear/scaling_law.m
@@ -26,26 +26,48 @@ function coefficients = scaling_law(N,degree);
 
 coefficients = zeros(degree,1);
 
-if degree == 1 % updated 3/23/2018 N=48
-    coefficients = exp(0.346140204693076) * N^-1.100982456346863;
+if degree == 1 % updated 4/13/2018 N=48
+    coefficients = exp(0.464178051931995) * N^-1.076854649494771;
 end
 
-if degree == 2 % updated 3/23/2018 N=48
-    coefficients(1) = exp(0.688731579483296) * N^-0.997313455364361;
-    coefficients(2) = exp(1.115048630933493) * N^-2.126965489206877;
+if degree == 2 % updated 4/13/2018 N=48
+    coefficients(1) = exp(0.895358015405417) * N^-0.998464070418845;
+    coefficients(2) = exp(1.543719708831006) * N^-2.135828666672578;
 end
 
-if degree == 3 % updated 3/23/2018 N=48
-    coefficients(1) = exp(0.870581358602445) * N^-0.997205857074403;
-    coefficients(2) = exp(1.638663408564890) * N^-2.114753283686792;
-    coefficients(3) = exp(1.722885571602311) * N^-3.289326488342230;
+if degree == 3 % updated 4/13/2018 N=48
+    coefficients(1) = exp(0.974697906498827) * N^-0.962070195645239;
+    coefficients(2) = exp(1.822700323654759) * N^-2.041713874437572;
+    coefficients(3) = exp(1.857104757767303) * N^-3.148049560074087;
 end
 
-if degree == 4 % updated 3/23/2018 N=48
-    coefficients(1) = exp(1.052501710233077) * N^-1.043950293373851;
-    coefficients(2) = exp(2.094977342459796) * N^-2.226203385845042;
-    coefficients(3) = exp(2.734550827044432) * N^-3.538251864483979;
-    coefficients(4) = exp(3.058650506815392) * N^-5.187724919959430;
+if degree == 4 % updated 4/13/2018 N=48
+    coefficients(1) = exp(1.135943524780088) * N^-0.985718175635504;
+    coefficients(2) = exp(2.222251934246004) * N^-2.089820199929696;
+    coefficients(3) = exp(2.826994626808396) * N^-3.289350267350072;
+    coefficients(4) = exp(2.868938248275644) * N^-4.623321597576260;
 end
+
+% if degree == 1 % updated 3/23/2018 N=48
+%     coefficients = exp(0.346140204693076) * N^-1.100982456346863;
+% end
+% 
+% if degree == 2 % updated 3/23/2018 N=48
+%     coefficients(1) = exp(0.688731579483296) * N^-0.997313455364361;
+%     coefficients(2) = exp(1.115048630933493) * N^-2.126965489206877;
+% end
+% 
+% if degree == 3 % updated 3/23/2018 N=48
+%     coefficients(1) = exp(0.870581358602445) * N^-0.997205857074403;
+%     coefficients(2) = exp(1.638663408564890) * N^-2.114753283686792;
+%     coefficients(3) = exp(1.722885571602311) * N^-3.289326488342230;
+% end
+% 
+% if degree == 4 % updated 3/23/2018 N=48
+%     coefficients(1) = exp(1.052501710233077) * N^-1.043950293373851;
+%     coefficients(2) = exp(2.094977342459796) * N^-2.226203385845042;
+%     coefficients(3) = exp(2.734550827044432) * N^-3.538251864483979;
+%     coefficients(4) = exp(3.058650506815392) * N^-5.187724919959430;
+% end
 
 
