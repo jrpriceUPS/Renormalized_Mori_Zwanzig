f7d68a2c73f2d573c8a9e195b5b5a884978a0c05
Jake Price <jrp14@uw.edu>
2018-04-24 12:26:40 -0700

    updated error plots

--- Burgers/Figures/error_test.m
+++ Burgers/Figures/error_test.m
@@ -1,4 +1,4 @@
-function time_array = error_test(N,alpha,endtime)
+function [errc1B,errc2B,errc3B,errc4B] = error_test(N,alpha,endtime)
 
 addpath ../simulation_functions
 addpath ../nonlinear
@@ -18,7 +18,7 @@ end
 leg = {'n = 1, constant coefficients','n = 2, constant coefficients','n = 3, constant coefficients','n = 4, constant coefficients','n = 1, decaying coefficients','n = 2, decaying coefficients','n = 3, decaying coefficients','n = 4, decaying coefficients'};
 
 
-time_array = zeros(4,2);
+
 
 simulation_params.N = N;
 simulation_params.alpha = alpha;
@@ -27,7 +27,7 @@ simulation_params.print_time = 1;
 simulation_params.time_range = t_list;
 
 % Burgers-style 1 ROM
-if ~(exist(sprintf('uc1B_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc1B_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c1B';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -35,17 +35,17 @@ if ~(exist(sprintf('uc1B_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 1;
     [tc1B,uc1B] = PDE_solve(simulation_params);
-    save(sprintf('uc1B_err%i.mat',endtime),'uc1B')
-    save(sprintf('tc1B_err%i.mat',endtime),'tc1B')
+    save(sprintf('uc1B_err%i_%i.mat',N,endtime),'uc1B')
+    save(sprintf('tc1B_err%i_%i.mat',N,endtime),'tc1B')
 else
-    load(sprintf('uc1B_err%i.mat',endtime))
-    load(sprintf('tc1B_err%i.mat',endtime))
+    load(sprintf('uc1B_err%i_%i.mat',N,endtime))
+    load(sprintf('tc1B_err%i_%i.mat',N,endtime))
 end
-time_array(1,1) = min(diff(tc1B));
+
 
 
 % Burgers-style 1+2 ROM
-if ~(exist(sprintf('uc2B_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc2B_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c2B';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -53,16 +53,16 @@ if ~(exist(sprintf('uc2B_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 1;
     [tc2B,uc2B] = PDE_solve(simulation_params);
-    save(sprintf('uc2B_err%i.mat',endtime),'uc2B')
-    save(sprintf('tc2B_err%i.mat',endtime),'tc2B')
+    save(sprintf('uc2B_err%i_%i.mat',N,endtime),'uc2B')
+    save(sprintf('tc2B_err%i_%i.mat',N,endtime),'tc2B')
 else
-    load(sprintf('uc2B_err%i.mat',endtime))
-    load(sprintf('tc2B_err%i.mat',endtime))
+    load(sprintf('uc2B_err%i_%i.mat',N,endtime))
+    load(sprintf('tc2B_err%i_%i.mat',N,endtime))
 end
-time_array(2,1) = min(diff(tc2B));
+
 
 % Burgers-style 1+2+3 ROM
-if ~(exist(sprintf('uc3B_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc3B_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c3B';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -70,16 +70,16 @@ if ~(exist(sprintf('uc3B_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 1;
     [tc3B,uc3B] = PDE_solve(simulation_params);
-    save(sprintf('uc3B_err%i.mat',endtime),'uc3B')
-    save(sprintf('tc3B_err%i.mat',endtime),'tc3B')
+    save(sprintf('uc3B_err%i_%i.mat',N,endtime),'uc3B')
+    save(sprintf('tc3B_err%i_%i.mat',N,endtime),'tc3B')
 else
-    load(sprintf('uc3B_err%i.mat',endtime))
-    load(sprintf('tc3B_err%i.mat',endtime))
+    load(sprintf('uc3B_err%i_%i.mat',N,endtime))
+    load(sprintf('tc3B_err%i_%i.mat',N,endtime))
 end
-time_array(3,1) = min(diff(tc3B));
+
 
 % Burgers-style 1+2+3+4 ROM
-if ~(exist(sprintf('uc4B_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc4B_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c4B';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -87,18 +87,18 @@ if ~(exist(sprintf('uc4B_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 1;
     [tc4B,uc4B] = PDE_solve(simulation_params);
-    save(sprintf('uc4B_err%i.mat',endtime),'uc4B')
-    save(sprintf('tc4B_err%i.mat',endtime),'tc4B')
+    save(sprintf('uc4B_err%i_%i.mat',N,endtime),'uc4B')
+    save(sprintf('tc4B_err%i_%i.mat',N,endtime),'tc4B')
 else
-    load(sprintf('uc4B_err%i.mat',endtime))
-    load(sprintf('tc4B_err%i.mat',endtime))
+    load(sprintf('uc4B_err%i_%i.mat',N,endtime))
+    load(sprintf('tc4B_err%i_%i.mat',N,endtime))
 end
-time_array(4,1) = min(diff(tc4B));
+
 
 
 
 % KdV-style 1 ROM
-if ~(exist(sprintf('uc1KdV_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc1KdV_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c1KdV';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -106,16 +106,16 @@ if ~(exist(sprintf('uc1KdV_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 0;
     [tc1KdV,uc1KdV] = PDE_solve(simulation_params);
-    save(sprintf('uc1KdV_err%i.mat',endtime),'uc1KdV')
-    save(sprintf('tc1KdV_err%i.mat',endtime),'tc1KdV')
+    save(sprintf('uc1KdV_err%i_%i.mat',N,endtime),'uc1KdV')
+    save(sprintf('tc1KdV_err%i_%i.mat',N,endtime),'tc1KdV')
 else
-    load(sprintf('uc1KdV_err%i.mat',endtime))
-    load(sprintf('tc1KdV_err%i.mat',endtime))
+    load(sprintf('uc1KdV_err%i_%i.mat',N,endtime))
+    load(sprintf('tc1KdV_err%i_%i.mat',N,endtime))
 end
-time_array(1,2) = min(diff(tc1KdV));
+
 
 % KdV-style 1+2 ROM
-if ~(exist(sprintf('uc2KdV_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc2KdV_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c2KdV';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -123,16 +123,16 @@ if ~(exist(sprintf('uc2KdV_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 0;
     [tc2KdV,uc2KdV] = PDE_solve(simulation_params);
-    save(sprintf('uc2KdV_err%i.mat',endtime),'uc2KdV')
-    save(sprintf('tc2KdV_err%i.mat',endtime),'tc2KdV')
+    save(sprintf('uc2KdV_err%i_%i.mat',N,endtime),'uc2KdV')
+    save(sprintf('tc2KdV_err%i_%i.mat',N,endtime),'tc2KdV')
 else
-    load(sprintf('uc2KdV_err%i.mat',endtime))
-    load(sprintf('tc2KdV_err%i.mat',endtime))
+    load(sprintf('uc2KdV_err%i_%i.mat',N,endtime))
+    load(sprintf('tc2KdV_err%i_%i.mat',N,endtime))
 end
-time_array(2,2) = min(diff(tc2KdV));
+
 
 % KdV-style 1+2+3 ROM
-if ~(exist(sprintf('uc3KdV_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc3KdV_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c3KdV';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -140,16 +140,16 @@ if ~(exist(sprintf('uc3KdV_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 0;
     [tc3KdV,uc3KdV] = PDE_solve(simulation_params);
-    save(sprintf('uc3KdV_err%i.mat',endtime),'uc3KdV')
-    save(sprintf('tc3KdV_err%i.mat',endtime),'tc3KdV')
+    save(sprintf('uc3KdV_err%i_%i.mat',N,endtime),'uc3KdV')
+    save(sprintf('tc3KdV_err%i_%i.mat',N,endtime),'tc3KdV')
 else
-    load(sprintf('uc3KdV_err%i.mat',endtime))
-    load(sprintf('tc3KdV_err%i.mat',endtime))
+    load(sprintf('uc3KdV_err%i_%i.mat',N,endtime))
+    load(sprintf('tc3KdV_err%i_%i.mat',N,endtime))
 end
-time_array(3,2) = min(diff(tc3KdV));
+
 
 % KdV-style 1+2+3+4 ROM
-if ~(exist(sprintf('uc4KdV_err%i.mat',endtime),'file') == 2)
+if ~(exist(sprintf('uc4KdV_err%i_%i.mat',N,endtime),'file') == 2)
     type = 'c4KdV';
     simulation_params.time_dependence = 1;
     simulation_params.degree = 4;
@@ -157,13 +157,13 @@ if ~(exist(sprintf('uc4KdV_err%i.mat',endtime),'file') == 2)
     simulation_params.initialization = @(x) ROM_init_Burgers(x);
     simulation_params.time_dependence = 0;
     [tc4KdV,uc4KdV] = PDE_solve(simulation_params);
-    save(sprintf('uc4KdV_err%i.mat',endtime),'uc4KdV')
-    save(sprintf('tc4KdV_err%i.mat',endtime),'tc4KdV')
+    save(sprintf('uc4KdV_err%i_%i.mat',N,endtime),'uc4KdV')
+    save(sprintf('tc4KdV_err%i_%i.mat',N,endtime),'tc4KdV')
 else
-    load(sprintf('uc4KdV_err%i.mat',endtime))
-    load(sprintf('tc4KdV_err%i.mat',endtime))
+    load(sprintf('uc4KdV_err%i_%i.mat',N,endtime))
+    load(sprintf('tc4KdV_err%i_%i.mat',N,endtime))
 end
-time_array(4,2) = min(diff(tc4KdV));
+
 
 
 
@@ -202,28 +202,20 @@ saveas(gcf,sprintf('Burgers_energy%i_%i',N,endtime),'png')
 close
 
 u_exact = u_list(1:N,:);
-errc1B = sum((uc1B - u_exact).*conj(uc1B - u_exact),1);
-errc2B = sum((uc2B - u_exact).*conj(uc2B - u_exact),1);
-errc3B = sum((uc2B - u_exact).*conj(uc2B - u_exact),1);
-errc4B = sum((uc2B - u_exact).*conj(uc2B - u_exact),1);
-errc1KdV = sum((uc1KdV - u_exact).*conj(uc1KdV - u_exact),1);
-errc2KdV = sum((uc2KdV - u_exact).*conj(uc2KdV - u_exact),1);
-errc3KdV = sum((uc3KdV - u_exact).*conj(uc3KdV - u_exact),1);
-errc4KdV = sum((uc4KdV - u_exact).*conj(uc4KdV - u_exact),1);
+errc1B = sum((uc1B(:,1:length(tc1B)) - u_exact(:,1:length(tc1B))).*conj(uc1B(:,1:length(tc1B)) - u_exact(:,1:length(tc1B))),1)./sum(u_exact(:,1:length(tc1B)).*conj(u_exact(:,1:length(tc1B))),1);
+errc2B = sum((uc2B(:,1:length(tc2B)) - u_exact(:,1:length(tc2B))).*conj(uc2B(:,1:length(tc2B)) - u_exact(:,1:length(tc2B))),1)./sum(u_exact(:,1:length(tc2B)).*conj(u_exact(:,1:length(tc2B))),1);
+errc3B = sum((uc3B(:,1:length(tc3B)) - u_exact(:,1:length(tc3B))).*conj(uc3B(:,1:length(tc3B)) - u_exact(:,1:length(tc3B))),1)./sum(u_exact(:,1:length(tc3B)).*conj(u_exact(:,1:length(tc3B))),1);
+errc4B = sum((uc4B(:,1:length(tc4B)) - u_exact(:,1:length(tc4B))).*conj(uc4B(:,1:length(tc4B)) - u_exact(:,1:length(tc4B))),1)./sum(u_exact(:,1:length(tc4B)).*conj(u_exact(:,1:length(tc4B))),1);
 
 figure(1)
 hold off
-plot(tc1B,errc1B,'r')
+plot(tc1B,errc1B,'r','linewidth',1.5)
 hold on
-plot(tc2B,errc2B,'k')
-plot(tc3B,errc3B,'c')
-plot(tc4B,errc4B,'m')
-
-plot(tc1KdV,errc1KdV,'r--','linewidth',1.2)
-plot(tc2KdV,errc2KdV,'k--','linewidth',1.2)
-plot(tc3KdV,errc3KdV,'c--','linewidth',1.2)
-plot(tc4KdV,errc4KdV,'m--','linewidth',1.2)
-legend(leg{:},'location','southwest')
+plot(tc2B,errc2B,'k','linewidth',1.5)
+plot(tc3B,errc3B,'c','linewidth',1.5)
+plot(tc4B,errc4B,'m','linewidth',1.5)
+axis([0,endtime,0,2])
+legend(leg{1:4},'location','northeast')
 
 title(sprintf('N = %i',N),'fontsize',16)
 xlabel('t')
--- Burgers/Figures/generate_comparisons.m
+++ Burgers/Figures/generate_comparisons.m
@@ -1,9 +1,11 @@
-clear all;close all;
+function error_array = generate_comparisons(N_list,endtime)
 
-N_list = 4:2:24;
+error_array = cell(length(N_list),4);
 
-t_array = zeros(4,2,length(N_list));
 for i = 1:length(N_list)
-    t = coeff_test(N_list(i),1,1000);
-    t_array(:,:,i) = t;
+    [errc1B,errc2B,errc3B,errc4B] = error_test(N_list(i),1,endtime);
+    error_array{i,1} = errc1B;
+    error_array{i,2} = errc2B;
+    error_array{i,3} = errc3B;
+    error_array{i,4} = errc4B;
 end
