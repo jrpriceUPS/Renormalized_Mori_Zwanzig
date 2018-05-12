ecfb16e54ed8b70495a24b4fa383d95f04b0e2c7
Jake Price <jrp14@uw.edu>
2018-05-11 17:14:08 -0700

    fixed some plotting errors

--- KdV/Simulations/constant_coeff_test_may11/constant_scaling_law_failure.m
+++ KdV/Simulations/constant_coeff_test_may11/constant_scaling_law_failure.m
@@ -1,4 +1,9 @@
-clear all;close all;
+%clear all;close all;
+
+
+addpath ../../simulation_functions
+addpath ../../nonlinear
+addpath ../../analysis
 
 N = 20;
 epsilon = 0.1;
@@ -35,7 +40,7 @@ simulation_params.epsilon = epsilon;  %coefficient on linear term in KdV
 simulation_params.alpha = 1;      %coefficient on nonlinear term in KdV
 simulation_params.dt = 1e-3;      %timestep
 simulation_params.endtime = 100;   %end of simulation
-simulation_params.howoften = 1;   %how often to save state vector
+simulation_params.howoften = 10;   %how often to save state vector
 simulation_params.blowup = 1;     %if 1, instabilities cause simulation to end, but not give error
 simulation_params.tol = inf;    %tolerance for identifying instabilities
 simulation_params.N = 256;          %number of positive modes to simulate
@@ -51,7 +56,7 @@ simulation_params.epsilon = epsilon;  %coefficient on linear term in KdV
 simulation_params.alpha = 1;      %coefficient on nonlinear term in KdV
 simulation_params.dt = 1e-3;      %timestep
 simulation_params.endtime = 100;   %end of simulation
-simulation_params.howoften = 1;   %how often to save state vector
+simulation_params.howoften = 10;   %how often to save state vector
 simulation_params.blowup = 1;     %if 1, instabilities cause simulation to end, but not give error
 simulation_params.tol = inf;    %tolerance for identifying instabilities
 simulation_params.initial_condition = @(x) sin(x);
@@ -74,14 +79,34 @@ exact = get_energy(u_list,N);
 all4 = get_energy(u_all,N);
 just24 = get_energy(u_24,N);
 
-figure
-
+figure(1)
+hold off
 plot(t_list,exact,'b')
 hold on
 plot(t_all,all4,'r')
 plot(t_24,just24,'k')
 title('N = 20','fontsize',16)
-legend('exact','t-model through t^4-model','only t^2-model and t^4-model','location','northwest')
+legend('exact','t-model through t^4-model','only t^2-model and t^4-model','location','northeast')
 xlabel('time','fontsize',16)
 ylabel('energy','fontsize',16)
+axis([0,100,0.499,0.51])
 saveas(gcf,'constant_coeff_energy','png')
+
+
+
+[x,u_real] = make_real_space(u_list(1:N,:),N);
+[~,u_all_real] = make_real_space(u_all,N);
+[~,u_24_real] = make_real_space(u_24,N);
+
+err_all = sum((u_real(:,1:length(t_all))-u_all_real).^2,1)./sum(u_real(:,1:length(t_all)).^2,1);
+err_24 = sum((u_24_real-u_real(:,1:length(t_24))).^2,1)./sum(u_real(:,1:length(t_24)).^2,1);
+
+figure(2)
+hold off
+plot(t_all,err_all,'r')
+plot(t_24,err_24,'k')
+legend('t-model through t^4-model','only t^2-model and t^4-model','location','northeast')
+xlabel('time','fontsize',16)
+ylabel('relative error','fontsize',16)
+title('N = 20','fontsize',16)
+
