dc4e97b9694097f6e28fdc043fb7b2b952dd3a28
Jake Price <jrp14@uw.edu>
2018-06-08 15:18:56 -0700

    added turn_times function

---
+++ Euler/figures/decay_begins.m
@@ -0,0 +1,19 @@
+function turn_times = decay_begins(N_list,percentage)
+
+turn_times = zeros(length(N_list),1);
+
+for i = 1:length(N_list)
+    
+    % plot the energy in some modes
+    load(sprintf('energy_%i_%i.mat',N,end_time))
+    load(sprintf('t4_%i_%i',N,end_time))
+    
+    
+    % compute slopes and energy draining times
+    turn_percent = percentage;
+    
+    energy_change = abs(energy-energy(1))/abs(energy(1));
+    turn_times(i) = t4(find(energy_change>turn_percent,1));
+    
+    
+end
