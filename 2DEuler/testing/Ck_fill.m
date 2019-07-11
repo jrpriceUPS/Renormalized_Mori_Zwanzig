3b69dabf939e67f12b969a1bbc392daf26bde6a8
m-shapiro <mshapiro@pugetsound.edu>
2019-07-11 14:47:39 -0700

    testing files for Ck problem

---
+++ 2DEuler/testing/Ck_fill.m
@@ -0,0 +1,54 @@
+function [C1,C2] = Ck_fill(irange,jrange,k,convo)
+%
+% A function to use the computed convolution of v and w to fill out the
+% array for Ck(v,w)
+%
+%
+%%%%%%%%%
+%INPUTS:%
+%%%%%%%%%
+%
+%  irange  =  the range of k_x values (positive, negative, or zero)
+%
+%  jrange  =  the range of k_y values (positive, negative, or zero)
+%
+%       k  =  the array of wavevectors (2Mx2Mx2)
+%
+%   convo  =  the convolution sum ( sum_{p+q=k} v_q*w_p^T )
+%
+%
+%%%%%%%%%%
+%OUTPUTS:%
+%%%%%%%%%%
+%
+%  C  =  the convolution C_k(v,w) for each Fourier mode in the range
+%            [irange,jrange,:]
+
+% find the size of the arrays we are using
+s = size(convo);
+
+
+t1 = zeros(s(1:end-1));
+k1 = repmat(k(irange,jrange,1),[1,1,2]);
+k2 = repmat(k(irange,jrange,2),[1,1,2]);
+t1(irange,jrange,:) = -1j*(k1.*convo(irange,jrange,:,1) + k2.*convo(irange,jrange,:,2));
+
+
+t2 = zeros(s(1:end-1));
+
+wave_vec_mag = sum(k(irange,jrange,:).^2,3);
+wave_vec_mag(wave_vec_mag==0)=1;
+wave_vec_mag = repmat(wave_vec_mag,[1,1,2]);
+
+t1_1 = repmat(t1(irange,jrange,1),[1,1,2]);
+t1_2 = repmat(t1(irange,jrange,2),[1,1,2]);
+
+B = k1.*t1_1 + k2.*t1_2;
+t2(irange,jrange,:) = k(irange,jrange,:).*B./wave_vec_mag;
+
+
+
+
+% retain only relevant terms
+C1 = t1(irange,jrange,:);
+C2 = -t2(irange,jrange,:);
---
+++ 2DEuler/testing/convolution_testing.m
@@ -0,0 +1,82 @@
+N = 2;
+M = 6;
+
+k = zeros(2*N,2*N,2);
+k(1,:,1) = 0;
+k(2,:,1) = 1;
+k(3,:,1) = -2;
+k(4,:,1) = -1;
+k(:,1,2) = 0;
+k(:,2,2) = 1;
+k(:,3,2) = -2;
+k(:,4,2) = -1;
+
+u = rand(2*N,2*N,2);
+%u(:,:,1) = [0 1i 2i 3i;4i 5i 6i 7i;8i 9i 10i 11i;12i 13i 14i 15i];
+%u(:,:,2) = [0 1i 2i 3i;4i 5i 6i 7i;8i 9i 10i 11i;12i 13i 14i 15i];
+
+v = rand(2*N,2*N,2);
+%v(:,:,1) = [0 -1i -2i -3i;-4i -5i -6i -7i;-8i -9i -10i -11i;-12i -13i -14i -15i];
+%v(:,:,2) = [0 -1i -2i -3i;-4i -5i -6i -7i;-8i -9i -10i -11i;-12i -13i -14i -15i];
+
+newu = u_squishify(u,N);
+newv = u_squishify(v,N);
+
+u = u_fullify(newu,N);
+v = u_fullify(newv,N);
+u_big = u_fullify(newu,M);
+v_big = u_fullify(newv,M);
+
+C1 = zeros(size(u));
+C2 = zeros(size(u));
+convo1 = 0;
+convo2 = 0;
+for i = 1:4
+    for j = 1:4
+        if i == 1 & j == 1
+        else
+            k_vec = squeeze(k(i,j,:));
+            for m = 1:4
+                for n = 1:4
+                    a_vec = squeeze(k(m,n,:));
+                    for p = 1:4
+                        for q = 1:4
+                            b_vec = squeeze(k(p,q,:));
+                            if (a_vec(1) + b_vec(1) == k_vec(1)) && (a_vec(2) + b_vec(2) == k_vec(2))
+                                u_vec = squeeze(u(m,n,:));
+                                v_vec = squeeze(v(p,q,:));
+                                A1 = eye(2);
+                                A2 = -(k_vec*k_vec')/(norm(k_vec)^2);
+                                d1 = dot(k_vec,u_vec)*(A1*v_vec);
+                                d2 = dot(k_vec,u_vec)*(A2*v_vec);
+                                
+                                convo1 = convo1 + -1i*d1;
+                                convo2 = convo2 + -1i*d2;
+                            end
+                        end
+                    end
+                end
+            end
+        end
+        C1(i,j,:) = convo1;
+        C2(i,j,:) = convo2;
+        convo1 = zeros(2,1);
+        convo2 = zeros(2,1);
+    end
+end
+C1;
+C2;
+% make k array
+kvec2 = [0:M-1,-M:1:-1];
+[kx,ky] = ndgrid(kvec2,kvec2);
+k2 = zeros(2*M,2*M,2);
+k2(:,:,1) = kx;
+k2(:,:,2) = ky;
+
+a = 2:M;
+b = 2*M:-1:M+2;
+a_tilde = N+1:M;
+a_tilde2 = 2*N+1:M;
+[C1_new,C2_new] = Ck(u_big,v_big,a,b,k2,a_tilde,a_tilde2);
+u_squishify(C1,N) - u_squishify(C1_new,N)
+u_squishify(C2,N) - u_squishify(C2_new,N)
